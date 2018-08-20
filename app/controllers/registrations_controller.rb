efef9b949ddfb7271234d57800c8c7254160cac8
XinHui <xhleong@hotmail.com>
2018-08-20 15:49:10 +0800

    setup devise linking to company routes and included additional user fields

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,41 @@
+class RegistrationsController < Devise::RegistrationsController
+    def new
+        @company = Company.find(params[:id])
+        build_resource
+        yield resource if block_given?
+        respond_with resource
+    end
+
+    def create
+        @company = Company.find(params[:id])
+        build_resource(sign_up_params)
+        resource.company_id = params[:id]
+        resource.save
+        yield resource if block_given?
+        if resource.persisted?
+          if resource.active_for_authentication?
+            set_flash_message! :notice, :signed_up
+            sign_up(resource_name, resource)
+            respond_with resource, location: after_sign_up_path_for(resource)
+          else
+            set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+            expire_data_after_sign_in!
+            respond_with resource, location: after_inactive_sign_up_path_for(resource)
+          end
+        else
+          clean_up_passwords resource
+          set_minimum_password_length
+          respond_with resource
+        end
+      end
+
+
+    protected
+    def sign_up_params
+        params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
+
+    def account_update_params
+        params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+    end
+end
